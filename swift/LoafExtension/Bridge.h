0f8d52d965917b51838876180145c8d62fa836f0
Andrew Gazelka <andrew.gazelka@gmail.com>
2025-12-24 14:25:12 -0600

    update

--- swift/LoafExtension/Bridge.h
+++
@@ -1,6 +0,0 @@
-#ifndef Bridge_h
-#define Bridge_h
-
-#include "loaf.h"
-
-#endif /* Bridge_h */
--- swift/LoafExtension/LoafExtension.swift
+++
@@ -1,9 +0,0 @@
-import Foundation
-import FSKit
-
-@main
-struct LoafExtension: UnaryFileSystemExtension {
-    var fileSystem: FSUnaryFileSystem & FSUnaryFileSystemOperations {
-        LoafFileSystem()
-    }
-}
